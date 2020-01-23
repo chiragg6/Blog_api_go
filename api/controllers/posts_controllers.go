407ddcba66675e9bca7dcc6cd8060f71ed9aa6fa
Chirag Gupta <chirag@aicumen.com>
2020-01-23 19:17:11 +0530

    Post Update function added

--- api/controllers/posts_controllers.go
+++ api/controllers/posts_controllers.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -78,4 +79,57 @@ func (server *Server) GetPost(w http.ResponseWriter, r *http.Request) {
 	}
 
 	responses.JSON(w, http.StatusOK, postRecieved)
+}
+
+func (server *Server) UpdatePost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	id := vars["id"]
+
+	post := models.Post{}
+
+	//check if the post exits
+	err := server.DB.Debug().Model(models.Post{}).Where("id = ?", id).Take(&post).Error
+	if err != nil {
+		// panic(err)
+		fmt.Println("Post Not Found", err)
+		return
+	}
+
+	if id != post.AuthorID {
+		fmt.Println("not authorised user")
+		return
+
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	// Request data - Update Procees
+
+	postUpdate := models.Post{}
+	err = json.Unmarshal(body, &postUpdate)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	postUpdate.Prepare()
+	err = postUpdate.Validate()
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	postUpdate.ID = post.ID //This is to maintain the original post id
+
+	UpdatedPost, err := postUpdate.UpdateAPost(server.DB)
+	if err != nil {
+		panic(err)
+		return
+	}
+	responses.JSON(w, http.StatusOK, UpdatedPost)
 }
