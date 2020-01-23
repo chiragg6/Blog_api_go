97c834c9e79c850777d4ccd05109f4843d1c8741
Chirag Gupta <chirag@aicumen.com>
2020-01-23 17:27:09 +0530

    Adding controllers

---
+++ api/controllers/base.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/crud_api/models"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+)
+
+type Server struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
+
+func (server *Server) Initalize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, DbName string) {
+	var err error
+	if Dbdriver == "postgres" {
+		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
+		server.DB, err = gorm.Open(Dbdriver, DBURL)
+		if err != nil {
+			fmt.Printf("Cannot connect to %s database", Dbdriver)
+			log.Fatal("This is the error:", err)
+		} else {
+			fmt.Printf("We are connected to the %s database", Dbdriver)
+		}
+	}
+
+	server.DB.Debug().AutoMigrate(&models.User{}, &models.Post{})
+	//Database Migration
+}
