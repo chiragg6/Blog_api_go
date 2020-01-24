84d11ccbac4afb892b9e457aafaba349d52aeb2f
Chirag Gupta <chirag@aicumen.com>
2020-01-24 21:33:50 +0530

    Metrics added

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -6,7 +6,8 @@ func (s *Server) initializeRoutes() {
 	s.Router.HandleFunc("/users/{id}", s.GetUser).Methods("GET")
 	s.Router.HandleFunc("/users/{id}", s.UpdateUser).Methods("PUT")
 	s.Router.HandleFunc("/users/{id}", s.DeleteUser).Methods("DELETE")
-
+	// s.Router.HandleFunc("/metrics", promhttp.Handler())
+	// s.Router.HandleFunc("/metrics", promhttp.Handler())
 	//Posts routes
 	s.Router.HandleFunc("/posts", s.createPost).Methods("POST")
 	s.Router.HandleFunc("/posts", s.GetPost).Methods("GET")
--- go.mod
+++ go.mod
@@ -8,5 +8,6 @@ require (
 	github.com/gorilla/mux v1.7.3
 	github.com/jinzhu/gorm v1.9.12
 	github.com/joho/godotenv v1.3.0
+	github.com/prometheus/client_golang v1.3.0
 	golang.org/x/crypto v0.0.0-20200117160349-530e935923ad
 )
